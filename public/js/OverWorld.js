c2b2856db2297639d7ed3fbe901dec20bf1ad672
snyderae13 <snyderae13@icloud.com>
2021-11-07 18:18:55 -0600

    added images but they're not working on localhost

---
+++ public/js/OverWorld.js
@@ -0,0 +1,19 @@
+class Overworld {
+    constructor(config) {
+        this.element = config.element;
+        this.canvas = this.element.querySelector(".game-canvas");
+        this.ctx = this.canvas.getContext("2d");
+
+    }
+
+    init () {
+        const image = new Image();
+        image.onload = () => {
+            this.ctx.drawImage(image,0,0)
+
+        };
+        image.src = "/images/fullGrass.png";
+    }
+
+
+}
